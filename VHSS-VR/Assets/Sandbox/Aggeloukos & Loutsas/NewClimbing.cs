9d042280236ca6109bb45d952ea9d14e59bf9367
AggelosTsil Official <jesusisatigerofficial@gmail.com>
2025-07-07 14:11:20 +0300

    Climbing now checks if player is climbing at fixed times Spyglass button is back and has a cute sound when you toggle it on

--- VHSS-VR/Assets/Sandbox/Aggeloukos & Loutsas/NewClimbing.cs
+++ VHSS-VR/Assets/Sandbox/Aggeloukos & Loutsas/NewClimbing.cs
@@ -27,6 +27,7 @@ public class NewClimbing : MonoBehaviour
     private Vector3 TravelPoint; //used to move the player
     public float sensitivity; //Affects climbing speed,change accordingly
     public bool local; //I dont remember honestly 
+    public float HangingCheckTimer; //This ticks down and checks if one of the hands is grabbing the ropes on 0
 
     // Start is called before the first frame update
     private void Start() {
@@ -55,6 +56,9 @@ public class NewClimbing : MonoBehaviour
         CurrentRHPosition = RightHand.position;
         CurrentLHPosition = Lefthand.position;
 
+        //<<Update Timer>>
+        HangingCheckTimer -= Time.deltaTime;
+
         //<<Grabbing magic>> !!!!!!LOGIC ISNT VALID AND NEEDS WORK BUT CODE IS FUNCTIONAL!!!!!!
         if (GrabRight.IsPressed()) {
             if (GrabRight.IsPressed() && GrabLeft.IsPressed()) {
@@ -62,13 +66,16 @@ public class NewClimbing : MonoBehaviour
                 Debug.Log("<color=green>BOTH HANDS GRABBING</color>");
             }else {
                 //RIGHT hand grabbing
-                TravelPoint.Set(CurrentRHPosition.z - PreviousRHPosition.z, -CurrentRHPosition.y + PreviousRHPosition.y, -CurrentRHPosition.x + PreviousRHPosition.x);
+                TravelPoint.Set(CurrentRHPosition.x - PreviousRHPosition.x, -CurrentRHPosition.y + PreviousRHPosition.y, -CurrentRHPosition.z + PreviousRHPosition.z);
                 Debug.Log("<color=yellow>Right hand</color>");
             }
         }else if (GrabLeft.IsPressed()) {
             //LEFT hand grabbing
-            TravelPoint.Set(CurrentLHPosition.z - PreviousLHPosition.z, -CurrentLHPosition.y + PreviousLHPosition.y, -CurrentLHPosition.x + PreviousLHPosition.x);
+            TravelPoint.Set(CurrentLHPosition.x - PreviousLHPosition.x, -CurrentLHPosition.y + PreviousLHPosition.y, -CurrentLHPosition.z + PreviousLHPosition.z);
             Debug.Log("<color=blue>Left hand</color>");
+        }else if (HangingCheckTimer <= 0) {
+            //Caught lacking! 
+            Debug.Log("<color=red>No hands grabbing, falling down :c</color>");
         }
         //<<Refreshing positions>>
         Body.Translate(TravelPoint * sensitivity, local ? Space.Self : Space.World); 
