f75da66ec1220e664bb890ccbe24c3f7bdcb0c38
georgethepallis <georgepallis03@gmail.com>
2025-07-12 16:31:26 +0300

    Added Outlines on the wheel and the worker during the explore activity. (In the changes there is a file called ARIS.FBX.meta, it will not leave, so I am adding it to the changes)

--- VHSS-VR/Assets/Sandbox/Aggeloukos & Loutsas/Exploration.cs
+++ VHSS-VR/Assets/Sandbox/Aggeloukos & Loutsas/Exploration.cs
@@ -8,16 +8,22 @@ public class Exploration : MonoBehaviour
     public Scenario Scenario;
     public GameObject ExploreArea;
     public Teleport Teleport;
+    public Outline OutlineWorker;
+    public Outline OutlineWheel;
     // Start is called before the first frame update
     void Start()
     {
         player.transform.position = ExploreArea.transform.position;
         Teleport.TeleportToPoint.Enable();
+        OutlineWorker.enabled = true;
+        OutlineWheel.enabled = true;
     }
 
 
     private void OnDisable() {
         //Teleport.TeleportToPoint.Disable();
+        OutlineWorker.enabled = false;
+        OutlineWheel.enabled = false;
     }
 
     private void RevealHotSpot(GameObject HotSpot) {
